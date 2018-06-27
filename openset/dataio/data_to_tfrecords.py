2a6c6ec9cb678d4ee48f19b6f3676c7bd43283c4
Abhinav Agarwalla <navabhi174@gmail.com>
2018-06-27 19:23:15 +0100

    Adding tfrecords converter

---
+++ openset/dataio/data_to_tfrecords.py
@@ -0,0 +1,77 @@
+from __future__ import print_function
+import glob
+import os, sys
+os.environ["CUDA_VISIBLE_DEVICES"] = ""
+from tqdm import tqdm
+import numpy as np
+
+from os.path import expanduser
+# from pathos.multiprocessing import ProcessPool as Pool
+from scipy.io import loadmat
+from scipy.misc import imresize, imread
+
+import tensorflow as tf
+
+def _float_feature(value):
+  return tf.train.Feature(int64_list=tf.train.FloatList(value=[value]))
+
+def _bytes_feature(value):
+  return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
+
+label_mapping = {'aeroplane': 0, 'bicycle': 1, 'bus': 2, 'car': 3,
+    'horse': 4, 'knife': 5, 'motorcycle': 6, 'person': 7, 'plant': 8,
+    'skateboard': 9, 'train': 10, 'truck': 11, 'other': 12}
+
+def load_image_label(addr):
+    img = imread(addr)
+    label = label_mapping[addr.split('/')[-2]]
+    return img.astype(np.float32), np.array(label).astype(np.int32)
+
+def write_record(img_data_path, train_filename, addrs, split):
+    writer = tf.python_io.TFRecordWriter(train_filename)
+    for idx in tqdm(range(len(addrs))):
+        # try:
+        img, label = load_image_label(addrs[idx])
+
+        # Create a feature
+        feature = {split + '/label': _bytes_feature(tf.compat.as_bytes(label.tostring())),
+                   split + '/image': _bytes_feature(tf.compat.as_bytes(img.tostring()))}
+        
+        # Create an example protocol buffer
+        example = tf.train.Example(features=tf.train.Features(feature=feature))
+        
+        # Serialize to string and write on the file
+        writer.write(example.SerializeToString())
+
+        # except:
+        #     print("Couldn't write to tf, exception raised..moving to next image")
+
+    writer.close()
+
+def write_data(img_data_path):
+    categories = os.listdir(img_data_path)[1:]
+
+    addrs = []
+    for i in categories:
+        addrs.extend(glob.glob(img_data_path + i + '/*'))
+
+    # val_addrs = [glob.glob(img_data_path + i + '/*') for i in patients[int(0.75*len(patients)):int(0.90*len(patients))]]
+    # test_addrs = [glob.glob(img_data_path + i + '/*') for i in patients[int(0.9*len(patients)):]]
+
+    n_shards = 16
+    addrs = np.array_split(np.array(addrs), n_shards)
+    filenames = [expanduser("~") + '/Desktop/RelayVision/data/validation_tfrecords/{}_{:0>3}_{:0>3}.tfrecords'.format('train', i, n_shards-1) for i in range(n_shards)]
+    # val_filenames = [expanduser("~") + '/domain_adaptation/eye_gaze/data/realMPII/{}_{:0>3}_{:0>3}.tfrecords'.format('val', i, n_shards-1) for i in range(n_shards)]
+    # test_filenames = [expanduser("~") + '/domain_adaptation/eye_gaze/data/realMPII/{}_{:0>3}_{:0>3}.tfrecords'.format('test', i, n_shards-1) for i in range(n_shards)]
+
+    write_record(img_data_path, filenames[0], addrs[0], 'train')
+    # p = Pool(n_shards)
+    # p.map(write_record, train_filenames, train_addrs, ['train' for i in range(n_shards)])
+    # p.map(write_record, val_filenames, val_addrs, ['val' for i in range(n_shards/2)])
+    # p.map(write_record, test_filenames, test_addrs, ['test' for i in range(n_shards/2)])
+    sys.stdout.flush()
+
+if __name__=="__main__":
+    home = expanduser("~")
+    img_data_path = home + '/Desktop/RelayVision/data/validation/'
+    write_data(img_data_path)
